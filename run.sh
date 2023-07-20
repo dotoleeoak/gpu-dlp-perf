275d9ec7090e0b9140fb55e9816c648dd2b34e83
Jaemin Choi <1dotolee@gmail.com>
2023-07-21 00:35:55 +0900

    feat: use resnet50 for test

--- run.sh
+++ run.sh
@@ -4,7 +4,9 @@ set -e
 for i in $(seq 1 $num_gpus); do
   echo "============== Running with $i GPUs =============="
   horovodrun -np $i -H $host:$i --timeline-filename /workspace/timeline/timeline_single_$i.json \
-    python3 /horovod/examples/pytorch/pytorch_mnist.py > /workspace/logs/single_$i.log
+    python3 /horovod/examples/pytorch/pytorch_imagenet_resnet50.py \
+    --train-dir ~/imagenet/cifar10/train --val-dir ~/imagenet/cifar10/test \
+    &> /workspace/logs/single_$i.log
   sleep 10
 done
 
