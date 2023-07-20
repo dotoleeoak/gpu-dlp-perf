ce8b06aa4ccb36527fdc2822c1192f92982f67c2
Jaemin Choi <1dotolee@gmail.com>
2023-07-20 23:33:32 +0900

    chore: save logs

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -e
 for i in $(seq 1 $num_gpus); do
   echo "============== Running with $i GPUs =============="
   horovodrun -np $i -H $host:$i --timeline-filename /workspace/timeline/timeline_single_$i.json \
-    python3 /horovod/examples/pytorch/pytorch_mnist.py
+    python3 /horovod/examples/pytorch/pytorch_mnist.py > /workspace/logs/single_$i.log
   sleep 10
 done
 
