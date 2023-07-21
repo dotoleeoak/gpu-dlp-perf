4664a6683f5c70c459e54f4f97518f5aeb4b52b8
Jaemin Choi <1dotolee@gmail.com>
2023-07-21 14:20:07 +0900

    chore: reduce epochs

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ for i in $(seq 1 $num_gpus); do
   echo "============== Running with $i GPUs =============="
   horovodrun -np $i -H $host:$i --timeline-filename /workspace/timeline/timeline_single_$i.json \
     python3 /horovod/examples/pytorch/pytorch_imagenet_resnet50.py \
-    --train-dir ~/imagenet/cifar10/train --val-dir ~/imagenet/cifar10/test \
+    --train-dir ~/imagenet/cifar10/train --val-dir ~/imagenet/cifar10/test --epochs 5 \
     &> /workspace/logs/single_$i.log
   sleep 10
 done
